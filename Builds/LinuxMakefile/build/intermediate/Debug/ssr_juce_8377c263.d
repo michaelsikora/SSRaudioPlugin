44bc7a1a8f1aaa3d7efd28d376bb88c0976251c2
michaelsikora <sikoraaudio@gmail.com>
2020-03-25 14:08:09 +0100

    updated gitignore

--- Builds/LinuxMakefile/build/intermediate/Debug/ssr_juce_8377c263.d
+++
@@ -1,4 +0,0 @@
-build/intermediate/Debug/ssr_juce_8377c263.o: ../../Source/ssr_juce.cpp \
- ../../Source/ssr_juce.h ../../ssr/apf/apf/pointer_policy.h \
- ../../ssr/apf/apf/parameter_map.h ../../ssr/apf/apf/stringtools.h \
- ../../ssr/apf/apf/iterator.h ../../ssr/apf/apf/math.h
